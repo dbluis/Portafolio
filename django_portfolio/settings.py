dbe159c2e631ac5d82be041f263591b7620f9384
dbluis <db.luis.mch@gmail.com>
2024-03-25 02:02:30 -0300

    Ignorar archivos dentro de la carpeta media

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -133,7 +133,7 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 MEDIA_URL = '/media/'
 
